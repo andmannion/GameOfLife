cd9f8fb263d2002139dcd2d362528977293a7644
Andrew Mannion <andrew.mannion@ucdcoonnect.ie>
2018-09-24 22:57:52 +0100

    Change package import command

--- src/ie/ucd/engAC/Main.java
+++ src/ie/ucd/engAC/Main.java
@@ -1,9 +1,9 @@
-package ie.ucd.engAC;
+package src.ie.ucd.engAC;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    	System.out.println("Hello");
+    	System.out.println("Hello, engAC");
     }
 }
