82e8617212d43f66302cd7ac4b9463da4d1c614b
ConchOD <conchuod@users.noreply.github.com>
2018-10-05 18:40:52 +0100

    PlayPanel.java template

---
+++ src/ie/ucd/engAC/UIScreens/PlayPanel.java
@@ -0,0 +1,15 @@
+package ie.ucd.engAC.UIScreens;
+
+import javax.swing.*;
+
+public class PlayPanel extends JPanel implements Runnable {
+    public PlayPanel(){
+        super();
+        //TODO
+    }
+
+    @Override
+    public void run() {
+        //TODO
+    }
+}
