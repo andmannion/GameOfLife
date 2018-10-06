e701782ddd34771d2aa4f2126f8029abfdb73c86
Andrew Mannion <andrew.mannion@ucdcoonnect.ie>
2018-10-06 14:34:06 +0100

    Add config factory class for new ActionCardType

---
+++ src/ie/ucd/engAC/LifeGameLogic/Cards/ActionCards/GetCashFromBankActionCard.java
@@ -0,0 +1,14 @@
+package ie.ucd.engAC.LifeGameLogic.Cards.ActionCards;
+
+public class GetCashFromBankActionCard extends ActionCard {
+	private int amountToPay;
+
+	public int getAmountToPay() {
+		return amountToPay;
+	}
+
+	public GetCashFromBankActionCard(int amountToPay) {
+		actionCardType = ActionCardType.GetCashFromBank;
+		this.amountToPay = amountToPay;
+	}
+}
