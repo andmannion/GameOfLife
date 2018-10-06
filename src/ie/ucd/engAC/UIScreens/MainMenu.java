009bfefe5200eea51b0267169e7ab463bbb7e16c
ConchOD <conchuod@users.noreply.github.com>
2018-10-06 13:47:03 +0100

    Starting to make a the play panel work

--- src/ie/ucd/engAC/UIScreens/MainMenu.java
+++ src/ie/ucd/engAC/UIScreens/MainMenu.java
@@ -1,21 +1,16 @@
 package ie.ucd.engAC.UIScreens;
 
 import ie.ucd.engAC.LifeGame;
-import java.lang.Boolean;
 import ie.ucd.engAC.UIScreens.UISubPanels.MainMenuButtonPanel;
 import ie.ucd.engAC.UIScreens.buttons.NewGameButton;
 import ie.ucd.engAC.UIScreens.buttons.PlayButton;
 import ie.ucd.engAC.UIScreens.buttons.QuitGameButton;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
+
 
 public class MainMenu extends JPanel implements ActionListener {
 
