d5647cad56beb25f2056679c3b701f4f2dd0ba1e
ConchOD <conchuod@users.noreply.github.com>
2018-10-21 21:45:34 +0100

    paydaysubstate

--- src/main/java/ie/ucd/engac/lifegamelogic/gamestatehandling/PaydaySubstate.java
+++ src/main/java/ie/ucd/engac/lifegamelogic/gamestatehandling/PaydaySubstate.java
@@ -2,13 +2,12 @@ package ie.ucd.engac.lifegamelogic.gamestatehandling;
 
 import ie.ucd.engac.messaging.*;
 
-public class PaydaySubstate implements GameState{
+public class PaydaySubstate implements GameState {
     public PaydaySubstate(){}
 
     @Override
     public void enter(GameLogic gameLogic) {
         gameLogic.getCurrentPlayer().addToBalance(1); //TODO get this value from cfg
-        exit(gameLogic);
     }
 
     @Override
