62b7fcf4de60d5e9e480ce7762f525e4c580900b
ConchOD <conchuod@users.noreply.github.com>
2018-10-31 15:37:45 +0000

    .keep

---
+++ src/test/java/ie/ucd/engac/lifegamelogic/gamestatehandling/GameLogicTest.java
@@ -0,0 +1,12 @@
+package ie.ucd.engac.lifegamelogic.gamestatehandling;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameLogicTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
