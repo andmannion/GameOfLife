cec201344acc79679126ac458622984576e9fd47
ConchOD <conchuod@users.noreply.github.com>
2018-11-14 13:19:17 +0000

    player test hull

---
+++ src/test/java/ie/ucd/engac/lifegamelogic/playerlogic/PlayerTest.java
@@ -0,0 +1,134 @@
+package ie.ucd.engac.lifegamelogic.playerlogic;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPlayerNumber() {
+    }
+
+    @Test
+    void getPlayerColour() {
+    }
+
+    @Test
+    void computeRetirementBonuses() {
+    }
+
+    @Test
+    void getCurrentLocation() {
+    }
+
+    @Test
+    void setCurrentLocation() {
+    }
+
+    @Test
+    void getNumberOfDependants() {
+    }
+
+    @Test
+    void addDependants() {
+    }
+
+    @Test
+    void getCareerPath() {
+    }
+
+    @Test
+    void setCareerPath() {
+    }
+
+    @Test
+    void getOccupationCard() {
+    }
+
+    @Test
+    void setOccupationCard() {
+    }
+
+    @Test
+    void getCurrentMoney() {
+    }
+
+    @Test
+    void addToBalance() {
+    }
+
+    @Test
+    void subtractFromBalance() {
+    }
+
+    @Test
+    void getNumberOfLoans() {
+    }
+
+    @Test
+    void getTotalLoansOutstanding() {
+    }
+
+    @Test
+    void addActionCard() {
+    }
+
+    @Test
+    void getActionCards() {
+    }
+
+    @Test
+    void getNumberOfActionCards() {
+    }
+
+    @Test
+    void getHouseCards() {
+    }
+
+    @Test
+    void addHouseCard() {
+    }
+
+    @Test
+    void getNumberOfHouseCards() {
+    }
+
+    @Test
+    void sellHouseCard() {
+    }
+
+    @Test
+    void setMaritalStatus() {
+    }
+
+    @Test
+    void getMaritalStatus() {
+    }
+
+    @Test
+    void getPendingBoardForkChoice() {
+    }
+
+    @Test
+    void setPendingBoardForkChoice() {
+    }
+
+    @Test
+    void getMovesRemaining() {
+    }
+
+    @Test
+    void setMovesRemaining() {
+    }
+}
