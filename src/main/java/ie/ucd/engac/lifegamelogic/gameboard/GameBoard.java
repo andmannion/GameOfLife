f84bbd576c1d4e0469c66e18978b64a46cb5c479
Andrew Mannion <andrew.mannion@ucdconnect.ie>
2018-12-20 01:11:16 +0000

    Group public and private methods together

--- src/main/java/ie/ucd/engac/lifegamelogic/gameboard/GameBoard.java
+++ src/main/java/ie/ucd/engac/lifegamelogic/gameboard/GameBoard.java
@@ -1,17 +1,10 @@
 package ie.ucd.engac.lifegamelogic.gameboard;
 
-import com.google.gson.*;
 import ie.ucd.engac.customdatastructures.UDAGraph;
-import ie.ucd.engac.gsonExtender.RuntimeTypeAdapterFactory;
 import ie.ucd.engac.lifegamelogic.Spinner;
-import ie.ucd.engac.lifegamelogic.gameboard.gameboardtiles.GameBoardContinueTile;
-import ie.ucd.engac.lifegamelogic.gameboard.gameboardtiles.GameBoardStopTile;
 import ie.ucd.engac.lifegamelogic.gameboard.gameboardtiles.GameBoardTile;
 import ie.ucd.engac.messaging.Tile;
-import org.jetbrains.annotations.NotNull;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -26,6 +19,9 @@ public class GameBoard {
 		spinningWheel = new Spinner();
 		
 		initialiseBoard();
+
+		// Call destructor on boardConfigHandler
+
 	}
 
 	public int spinTheWheel() {
@@ -56,28 +52,28 @@ public class GameBoard {
 		return null;
 	}
 
+	/**
+	 * Get the board's layout
+	 * @return the layout of the board
+	 */
+	public ArrayList<Tile> getLayout(){
+		ArrayList<Tile> tiles = null;
+		try {
+			tiles = convertGameBoardTilesToMessageTiles();
+		}
+		catch (Exception e){
+			System.err.println("Could not get board locations from config file. " + e.toString());
+		}
+		return tiles;
+	}
+
 	private void initialiseBoard() {
 		boardConfigHandler.initialiseBoard();
 		boardGraph = boardConfigHandler.getBoardGraph();
 		idToGameBoardTileMap = boardConfigHandler.getIDGameBoardTileMap();
 	}
 
-    /**
-     * Get the board's layout
-     * @return the layout of the board
-     */
-	public ArrayList<Tile> getLayout(){
-	    ArrayList<Tile> tiles = null;
-	    try {
-            tiles = convertGameBoardTilesToMessageTiles();
-        }
-        catch (Exception e){
-	        System.err.println("Could not get board locations from config file. " + e.toString());
-        }
-        return tiles;
-    }
-
-    private ArrayList<Tile> convertGameBoardTilesToMessageTiles(){
+	private ArrayList<Tile> convertGameBoardTilesToMessageTiles(){
 	    ArrayList<Tile> tiles = new ArrayList<>();
         for (GameBoardTile gameBoardTile:idToGameBoardTileMap.values()){
             tiles.add(new Tile(gameBoardTile));
